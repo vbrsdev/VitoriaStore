c9cc6804e3b8c8ff08474f5d1eb34ad2854c4ece
Vitória Beatriz <vbrsvitoria@gmail.com>
2024-12-05 21:31:55 -0300

    Adicinando classes Product. Adicionado controller product

---
+++ src/VitoriaStore.API/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VitoriaStore.API.Controllers
+{
+    [Route("api/[controller]")] //localhost:5232/api/Product 
+    [ApiController]   //atributos
+    public class ProductController : ControllerBase //obrigatorio
+    {
+        [HttpGet]
+        //é uma interface que aceita qualquer tipo retorno
+        public IActionResult GetAllProducts()
+        {
+            return Ok("Trazendo todos os produtos");
+        }
+    }
+}
---
+++ src/VitoriaStore.API/Entities/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace VitoriaStore.API.Entities.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
+        public int Stock { get; set; }
+    }
+}

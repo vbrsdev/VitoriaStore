a9c3625d849ad3cd55121e2a55ef0b72f257080a
Vitória Beatriz <vbrsvitoria@gmail.com>
2025-04-27 11:45:00 -0300

    feat:adicionando CRUD a entidade Pessoa

---
+++ src/VitoriaStore.API/Entities/Persons/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace VitoriaStore.API.Entities.Persons
+{
+    //criando classe Person CRUD
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreateBy { get; set; }
+        public DateTime UpdateBy { get; set; }
+
+    }
+}
